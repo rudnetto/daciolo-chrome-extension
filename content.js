c3aed6cffd54ebadae1989b20f024b42aaf23d36
Rudney Netto <rudneynetto@gmail.com>
2018-10-11 11:42:23 -0300

    feat: replace DOM text values appending "a deuxxx"

--- content.js
+++ content.js
@@ -1,1 +1,19 @@
-alert('Glória a deuxxx');
+var elements = document.getElementsByTagName('*');
+
+for(i = 0; i < elements.length; i++) {
+  var element = elements[i];
+
+  for(j = 0; j < element.childNodes.length; j++) {
+    var node = element.childNodes[j];
+
+    if(node.nodeType === 3) {
+      var text = node.nodeValue;
+      var regex = /(Gloria|Glória|gloria|glória)+/g
+      var replacedText = text.replace(regex, '$& a deuxxx');
+
+      if (replacedText !== text) {
+        element.replaceChild(document.createTextNode(replacedText), node);
+      }
+    }
+  }
+}
