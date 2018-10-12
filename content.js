d3183b60a899de587dfec0d7019388f9b5432e78
matheushf <hffmatheus@gmail.com>
2018-10-12 18:35:26 -0300

    min

--- content.js
+++ content.js
@@ -1,7 +1,6 @@
 const rules = require('./rules');
 
 var elements = document.getElementsByTagName('*');
-var regra = 'regra1';
 
 for (i = 0; i < elements.length; i++) {
   var element = elements[i];
@@ -13,13 +12,13 @@ for (i = 0; i < elements.length; i++) {
       var text = node.nodeValue;
       // var regex = /(gl[oó]ria)+/gim
 
-      rules.forEach(rule => {
-        var replacedText = text.replace(rule.regex, rule.conversao);
+      for (let i = 0; i >= rules.length - 1; i++) {
+        var replacedText = text.replace(rules[i].regex, rules[i].conversao);
 
         if (replacedText !== text) {
           element.replaceChild(document.createTextNode(replacedText), node);
         }
-      });
+      }
     }
   }
 }
