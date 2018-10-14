9f0aafacc46c2c7dfe2883612faf2c8583beb9f1
Mark Riedesel <mark@klowner.com>
2018-10-13 17:38:42 -0700

    add webpack, use npm jquery, and add package.json - issue #14

--- rules.js
+++ rules.js
@@ -1,4 +1,4 @@
-var RULES = [
+export const RULES = [
   {
     nome: 'gloria',
     descricao: '',
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = (env, options) => ({
+	mode: 'development',
+	entry: {
+		app: './content.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\js$/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					}
+				}
+			}
+		]
+	},
+	output: {
+		filename: '[name].bundle.js',
+		path: path.join(__dirname, 'build'),
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'all',
+			maxSize: 0,
+			cacheGroups: {
+				commons: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendor',
+					chunks: 'all',
+				}
+			}
+		}
+	},
+
+})
